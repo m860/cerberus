8163d6253d47bd8fd4bc6c8a1d83150c5f3c9695
jean <jean.ma.1986@gmail.com>
2019-04-02 16:03:42 +0800

    build: 发布版本时会触发钉钉消息

---
+++ scripts/notify.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+code=$(curl https://oapi.dingtalk.com/robot/send?access_token=42b802e52335fe36c884c4a944eb42831263bdb26eda3ca163863fe45d9fc3d6 \
+        -H "Accept: application/json"  \
+        -H "Content-Type: application/json"  \
+        -d "{\"msgtype\":\"markdown\",\"markdown\":{\"title\":\"@ibuild-community/react-native-applet组件更新\",\"text\":\"具体内容请查看[tags](http://cmisgitlab01/ibuild-app/react-native-applet/tags)\"}}" \
+        --output /dev/null \
+        --progress-bar \
+        --write-out %{http_code})
+if [[ ${code} -ne 200 ]];then
+    echo "消息发送失败"
+    exit code
+fi
