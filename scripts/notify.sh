053ed88ed46800aefcf035110ef2e160db37a391
jean <jean.ma.1986@gmail.com>
2019-04-02 16:08:49 +0800

    chore: 优化钉钉消息

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -3,7 +3,7 @@
 code=$(curl https://oapi.dingtalk.com/robot/send?access_token=42b802e52335fe36c884c4a944eb42831263bdb26eda3ca163863fe45d9fc3d6 \
         -H "Accept: application/json"  \
         -H "Content-Type: application/json"  \
-        -d "{\"msgtype\":\"markdown\",\"markdown\":{\"title\":\"@ibuild-community/react-native-applet组件更新\",\"text\":\"具体内容请查看[tags](http://cmisgitlab01/ibuild-app/react-native-applet/tags)\"}}" \
+        -d "{\"msgtype\":\"markdown\",\"markdown\":{\"title\":\"react-native-applet更新\",\"text\":\"@ibuild-community/react-native-applet组件更新,具体内容请查看[tags](http://cmisgitlab01/ibuild-app/react-native-applet/tags)\"}}" \
         --output /dev/null \
         --progress-bar \
         --write-out %{http_code})
