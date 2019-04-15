074c92266109a823944c9e065fd851bd84518474
jean <jean.ma.1986@gmail.com>
2019-04-15 17:36:46 +0800

    fix: 修复Toast不传icon.size报错的问题

--- components/Toast.js
+++ components/Toast.js
@@ -3,6 +3,18 @@ import ToastUtils from "@react-native-pure/toast"
 import Icon from "react-native-vector-icons/FontAwesome"
 import type {ToastOption} from "./Types";
 
+function getIconOption(option: ToastOption) {
+    let iconProps = {};
+    if (option.icon) {
+        if (option.icon.size) {
+            iconProps.size = option.icon.size;
+        }
+        if (option.icon.color) {
+            iconProps.color = option.icon.color;
+        }
+    }
+    return iconProps;
+}
 
 export default {
     show: ToastUtils.show,
@@ -10,7 +22,7 @@ export default {
         ToastUtils.show({
             ...option,
             renderIcon: () => {
-                return <Icon name="times-circle" size={option.icon.size} color={option.icon.color}/>
+                return <Icon name="times-circle" {...getIconOption(option)}/>
             }
         })
     },
@@ -18,7 +30,7 @@ export default {
         ToastUtils.show({
             ...option,
             renderIcon: () => {
-                return <Icon name="check-circle" size={option.icon.size} color={option.icon.color}/>
+                return <Icon name="check-circle" {...getIconOption(option)}/>
             }
         })
     },
@@ -26,7 +38,7 @@ export default {
         ToastUtils.show({
             ...option,
             renderIcon: () => {
-                return <Icon name="warning" size={option.icon.size} color={option.icon.color}/>
+                return <Icon name="warning" {...getIconOption(option)}/>
             }
         })
     }
