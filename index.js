9ea8486955d3cd56349d3d48b5214d8c0101e954
m860 <jean.ma.1986@gmail.com>
2019-05-27 11:41:53 +0800

    feat: 导出Button

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export {default} from "./components/Applet"
 export {default as ToastUtils} from "./components/Toast"
 export {mutateImageComponent, openImagePicker, alert, confirm} from "./components/Utils"
+export {default as Button} from "./components/button/Button"
