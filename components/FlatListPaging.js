450cc98990d4ff273486bec8b4dc7e0a13716028
m860 <jean.ma.1986@gmail.com>
2019-05-27 10:08:13 +0800

    refactor(FlatListPaging): 优化部分逻辑，对属性名字做了调整
    BREAKING CHANGE: 1.加载流程进行了优化 2.属性名字做了调整

--- components/FlatListPaging.js
+++ components/FlatListPaging.js
@@ -4,58 +4,86 @@
  */
 
 import * as React from "react"
-import {
-    FlatList,
-    View,
-    Text,
-    ActivityIndicator,
-    StyleSheet
-} from "react-native"
+import {ActivityIndicator, FlatList, StyleSheet, Text, View} from "react-native"
 
 
-type LoadMoreStatus = {
-    normal:1,
-    loading:2,
-    noMore:3
-}
+const LoadMoreStatus = {
+    normal: 1,
+    loading: 2,
+    noMore: 3
+};
 
 type FlatListProps = {
     dataSource: Array,
-    renderItem: () => React.ReactElement < any >,
+    renderItem: () => React.ReactElement<any>,
     onPageChange: (pageIndex: number) => void,
-    totalRecords : number, /**总页数**/
-    pageIndex:number,
+    totalRecords: number, /**总页数**/
+    pageIndex: number,
     startPageNum: number, /**页码开始位置**/
-    initialPageSize: number,  /**每页的item个数**/
-    enabledPageEmptyView? : boolean,
-    renderSeparator? : () => React.ReactElement < any >,
-    renderHeader? : () => React.ReactElement < any >,
-    renderEmptyView? : () => React.ReactElement < any >,
-    renderFooter?:(status:$Values< typeof LoadMoreStatus>)=>React.ReactElement < any >,
-    indicatorColor? : string,
-    style? : any,
-    initialNumToRender? : number,
-    extraData? : any,
-    keyExtractor?:(item: Object, index: number) => string,
-    enableRefresh:boolean /**是否支持下拉刷新，默认支持**/
+    pageSize: number, /**每页的item个数**/
+    renderSeparator?: () => React.ReactElement<any>,
+    renderHeader?: () => React.ReactElement<any>,
+    renderEmpty?: () => React.ReactElement<any>,
+    renderFooter?: (status: $Values<typeof LoadMoreStatus>)=>React.ReactElement<any>,
+    indicatorColor?: string,
+    style?: any,
+    initialNumToRender?: number,
+    extraData?: any,
+    keyExtractor?: (item: Object, index: number) => string,
+    enableRefresh: boolean /**是否支持下拉刷新，默认支持**/
 }
 
 type FlatListState = {
-    refreshing:boolean,
-    loadMore:boolean
+    refreshing: boolean,
+    loadMore: boolean
 }
 
+const styles = StyleSheet.create({
+    empty: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    loadingView: {
+        flex: 1,
+        height: 80,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    noMoreDataView: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        height: 80,
+        borderTopWidth: StyleSheet.hairlineWidth,
+        borderTopColor: "#EEEEEE",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        borderBottomColor: "#EEEEEE",
+    },
+    noMoreDataText: {
+        marginLeft: 10,
+        marginRight: 10,
+        fontSize: 14,
+        color: '#9B9B9B'
+    }
+});
 
-export default class FlatListPaging extends React.Component<FlatListProps,FlatListState> {
+export default class FlatListPaging extends React.PureComponent<FlatListProps, FlatListState> {
 
     static defaultProps = {
-        enabledPageEmptyView: true,
         totalRecords: -1,
         indicatorColor: 'silver',
-        enableRefresh:true,
+        enableRefresh: true,
         emptyStyle: {
             flex: 1
-        }
+        },
+        renderEmpty: () => (
+            <View style={styles.empty}>
+                <Text>
+                    暂无数据
+                </Text>
+            </View>
+        )
     }
 
     constructor(props) {
@@ -66,32 +94,26 @@ export default class FlatListPaging extends React.Component<FlatListProps,FlatLi
         };
     }
 
-
-    componentDidMount(){
-        this._onRefresh()
-    }
-
-    componentWillReceiveProps(nextProps){
-        if(nextProps.pageIndex !== this.props.pageIndex &&
-            nextProps.pageIndex === this.props.startPageNum &&
-            !this.state.refreshing){
-            this._onRefresh()
-        }
-        else if(
-            nextProps.pageIndex !== this.props.pageIndex &&
-            nextProps.pageIndex !== this.props.startPageNum &&
-            !this.state.loadMore ){
-            this._onEndReached()
-        }
-    }
+    // componentWillReceiveProps(nextProps) {
+    //     if (nextProps.pageIndex !== this.props.pageIndex &&
+    //         nextProps.pageIndex === this.props.startPageNum &&
+    //         !this.state.refreshing) {
+    //         this._onRefresh()
+    //     } else if (
+    //         nextProps.pageIndex !== this.props.pageIndex &&
+    //         nextProps.pageIndex !== this.props.startPageNum &&
+    //         !this.state.loadMore) {
+    //         this._onEndReached()
+    //     }
+    // }
 
 
     get isEnd() {
-        let totalRecords = this.props.totalRecords || 0
+        let totalRecords = this.props.totalRecords || 0;
         if (totalRecords === 0) {
             return true;
         }
-        if (Math.ceil(totalRecords / this.props.initialPageSize) <= (this.props.pageIndex || this.props.startPageNum )) {
+        if (Math.ceil(totalRecords / this.props.pageSize) <= (this.props.pageIndex || this.props.startPageNum)) {
             return true;
         }
         return false;
@@ -99,24 +121,14 @@ export default class FlatListPaging extends React.Component<FlatListProps,FlatLi
     }
 
     _renderEmpty = () => {
-        if (this.props.enabledPageEmptyView && !this.state.refreshing && this.props.totalRecords === 0) {
-
-            if (this.props.renderEmptyView) {
-                return this.props.renderEmptyView()
-            }
-            return (
-                <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                    <Text>
-                        暂无数据
-                    </Text>
-                </View>
-            )
+        if (!this.state.refreshing && this.props.totalRecords === 0) {
+            return this.props.renderEmpty();
         }
         return null
     }
 
     _onRefresh = async () => {
-        if(!this.props.enableRefresh){
+        if (!this.props.enableRefresh) {
             return
         }
         let dateTime = new Date().getTime()
@@ -128,7 +140,7 @@ export default class FlatListPaging extends React.Component<FlatListProps,FlatLi
                 this.setState({
                     refreshing: false
                 })
-            }, Math.max(0,800- ((new Date()).getTime() - dateTime)))
+            }, Math.max(0, 800 - ((new Date()).getTime() - dateTime)))
 
         });
     }
@@ -143,7 +155,7 @@ export default class FlatListPaging extends React.Component<FlatListProps,FlatLi
         this.setState({
             loadMore: true
         }, async () => {
-            await this.props.onPageChange((this.props.pageIndex || this.props.startPageNum )+1);
+            await this.props.onPageChange((this.props.pageIndex || this.props.startPageNum) + 1);
             this.setState({
                 loadMore: false
             })
@@ -153,11 +165,11 @@ export default class FlatListPaging extends React.Component<FlatListProps,FlatLi
     _renderListFooter = () => {
         if (this.state.loadMore) {
 
-            if (this.props.renderFooter && this.props.renderFooter(LoadMoreStatus.loading)){
+            if (this.props.renderFooter && this.props.renderFooter(LoadMoreStatus.loading)) {
                 return this.props.renderFooter(LoadMoreStatus.loading)
             }
             return (
-                <View style={{flex: 1, height: 80, justifyContent: "center", alignItems: "center"}}>
+                <View style={styles.loadingView}>
                     <ActivityIndicator color={this.props.indicatorColor}/>
                 </View>
             )
@@ -168,35 +180,13 @@ export default class FlatListPaging extends React.Component<FlatListProps,FlatLi
             }
 
             return (
-                <View style={[{
-                    flexDirection: "row",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    height: 80
-                }]}>
-                    <View style={{
-                        flex: 1,
-                        height: StyleSheet.hairlineWidth,
-                        backgroundColor: '#EEEEEE'
-
-                    }}/>
-                    <Text style={{
-                        marginLeft: 10,
-                        marginRight: 10,
-                        fontSize: 14,
-                        color: '#9B9B9B'
-                    }}>没有更多数据了</Text>
-                    <View style={{
-                        flex: 1,
-                        height: StyleSheet.hairlineWidth,
-                        backgroundColor: '#EEEEEE'
-                    }}/>
+                <View style={styles.noMoreDataView}>
+                    <Text style={styles.noMoreDataText}>没有更多数据了</Text>
                 </View>
             )
-        }
-        else {
-            if (this.props.renderFooter && this.props.renderFooter(LoadMoreStatus.nomal)) {
-                return this.props.renderFooter(LoadMoreStatus.nomal)
+        } else {
+            if (this.props.renderFooter && this.props.renderFooter(LoadMoreStatus.normal)) {
+                return this.props.renderFooter(LoadMoreStatus.normal)
             }
             return null
         }
@@ -204,23 +194,25 @@ export default class FlatListPaging extends React.Component<FlatListProps,FlatLi
 
     render() {
         return (
-            <FlatList
-                style={this.props.style}
-                keyExtractor={this.props.keyExtractor}
-                ListEmptyComponent={this._renderEmpty}
-                refreshing={this.state.refreshing}
-                ItemSeparatorComponent={this.props.renderSeparator}
-                data={this.props.dataSource}
-                extraData={this.props.extraData}
-                renderItem={this.props.renderItem}
-                onRefresh={this._onRefresh}
-                onEndReached={this._onEndReached}
-                onEndReachedThreshold={0.5}
-                refreshing={this.state.refreshing}
-                initialNumToRender={this.props.initialNumToRender}
-                ListFooterComponent={this._renderListFooter}
-                ListHeaderComponent={this.props.renderHeader}
+            <FlatList style={this.props.style}
+                      keyExtractor={this.props.keyExtractor}
+                      ListEmptyComponent={this._renderEmpty}
+                      refreshing={this.state.refreshing}
+                      ItemSeparatorComponent={this.props.renderSeparator}
+                      data={this.props.dataSource}
+                      extraData={this.props.extraData}
+                      renderItem={this.props.renderItem}
+                      onRefresh={this._onRefresh}
+                      onEndReached={this._onEndReached}
+                      onEndReachedThreshold={0.5}
+                      initialNumToRender={this.props.initialNumToRender}
+                      ListFooterComponent={this._renderListFooter}
+                      ListHeaderComponent={this.props.renderHeader}
             />
         )
+    }
+
+    async componentDidMount() {
+        await this._onRefresh()
     }
 };
