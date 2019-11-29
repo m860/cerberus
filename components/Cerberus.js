a071883b5bcad0db13b76a2692e2fbd0270ba060
m860 <jean.ma.1986@gmail.com>
2019-11-29 14:44:54 +0800

    chore: stash

--- components/Cerberus.js
+++ components/Cerberus.js
@@ -2,3 +2,33 @@
  * @flow
  * @author Jean.h.ma 2019-11-29
  */
+import * as React from "react"
+import * as ReactNative from "react-native"
+import type {CerberusOption, CerberusState} from "../hooks/useCerberus";
+
+export type CerberusProps = {
+    option: CerberusOption,
+    onChange?: (status: CerberusState, defined: any)=>?React.Element<*>
+};
+
+type State = {
+    code: ?string
+};
+
+export class Cerberus extends React.PureComponent<CerberusProps, State> {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        const {
+            onChange
+        } = this.props;
+        if (onChange) {
+            return onChange()
+        }
+        return null;
+    }
+}
