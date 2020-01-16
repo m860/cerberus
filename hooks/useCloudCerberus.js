16f60c2cddccb2c3492ee7ae9619b8cdeecae027
m860 <jean.ma.1986@gmail.com>
2020-01-16 15:04:58 +0800

    fix: 支持multiple entry

--- hooks/useCloudCerberus.js
+++ hooks/useCloudCerberus.js
@@ -9,20 +9,33 @@ import client from "../libs/client"
 import {gql} from "apollo-boost"
 
 export type CloudCerberusProps = $Diff<CerberusOption, {| entry: any, debug: any, hash: any |}> & {
-    secret: ?string
+    secret: ?string,
+    /**
+     * 查询需要使用的entry，默认情况返回第一个entry
+     */
+    queryEntry?: (entries: Array<string>)=>string | null
 }
 
 export function useCloudCerberus(props: CloudCerberusProps): CerberusResult {
     const {
         secret,
+        queryEntry = (entries: Array<string>): string | null => {
+            if (entries && entries.length > 0) {
+                return entries[0];
+            }
+            return null;
+        },
         ...rest
     } = props;
 
 
-    const [bundle, setBundle] = React.useState<{| entry: ?string, hash: ?string |}>({entry: null, hash: null});
+    const [bundle, setBundle] = React.useState<{| entry: Array<string> | null, hash: ?string |}>({
+        entry: null,
+        hash: null
+    });
     const [status, defined, setStatus] = useCerberus({
         ...rest,
-        entry: bundle.entry,
+        entry: queryEntry(bundle && bundle.entry ? bundle.entry : []),
         debug: false,
         hash: bundle.hash
     });
