b2c81ae4cdd4bd195b5ce56d9951d27c083d04cc
m860 <jean.ma.1986@gmail.com>
2020-07-23 10:19:59 +0800

    refactor: 简化CerberusContextProvider的配置

--- components/CerberusContextProvider.js
+++ components/CerberusContextProvider.js
@@ -3,13 +3,11 @@
  * @author Jean.h.ma 2020/6/9
  */
 import * as React from "react"
-import ApolloClient from "apollo-boost";
 
 const Context = React.createContext<CerberusContext>({
-    client: new ApolloClient({
-        uri: `http://39.99.175.213/graphql`,
-        fetch: fetch
-    })
+    client: {
+        uri: `http://39.99.175.213/graphql`
+    }
 });
 
 export default Context;
--- hooks/useBundle.js
+++ hooks/useBundle.js
@@ -6,12 +6,17 @@
 import {gql} from "apollo-boost";
 import * as React from "react"
 import Context from "../components/CerberusContextProvider";
+import ApolloClient from "apollo-boost";
 
 export default function () {
     const context = React.useContext(Context);
+    const client = new ApolloClient({
+        fetch: fetch,
+        ...context.client
+    });
     return {
         getBundle: (secret: string): Promise<Bundle> => {
-            return context.client.query({
+            return client.query({
                 query: gql`
                     query bundle($secret:String!){
                         bundle(secret:$secret){
@@ -28,7 +33,7 @@ export default function () {
             }).then(({data}) => data.bundle);
         },
         bundleCount: (bundleID: string) => {
-            return context.client.mutate({
+            return client.mutate({
                 mutation: gql`
                     mutation bundleCount($bundleID:String){
                         bundleCount(bundleID: $bundleID)
